e14bc43957fadfa862c1e9f5f4776fb0a258a035
Robert Ramirez <robertpramirez44@gmail.com>
2021-12-18 19:41:03 -0700

    Initialized React App Configuration
    -Executed npm init to create a package.json file
    -Installed React packages: react, react-dom, react-router-dom, react-scripts
    -Installed Webpack packages: webpack, webpack-cli, webpack-dev-server, html-webpack-plugin
    -Installed Babel packages: @babel/core, @babel/preset-env, @babel/preset-react, babel-loader
    -Configured webpack.config.js, babelrc, index.js, App.js
    -Configured index.html

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Github Followers</h1>
+      </div>
+    );
+  }
+}
