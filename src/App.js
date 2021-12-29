7c6e49d4c7cf0107030dc4174864d75eec45ac76
Robert Ramirez <robertpramirez44@gmail.com>
2021-12-29 10:53:36 -0700

    Refactored App component from a class component to a functional component
    -Coverted the App component to a functional component to allow for use of hooks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Search from "./components/Search";
 import Followers from "./components/Followers";
 
-export default class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Search />} />
-          <Route path="followers" element={<Followers />} />
-        </Routes>
-      </BrowserRouter>
-    );
-  }
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Search />} />
+        <Route path="followers" element={<Followers />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
